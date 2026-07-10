443e97fb131e8910f2e9b455947f719925f640b1
Ehi-Orih <ehiorih25@gmail.com>
2026-07-10 11:50:56 +0100

    make reset button functional

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let score = {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+};
+
+
+
 /* get buttons for the moves from the html and give them onclick events corresponding to the moves*/
 const rock = document.getElementById("rock").onclick = () => {
     makeMove("rock")
@@ -8,6 +16,12 @@ const paper = document.getElementById("paper").onclick = () => {
 const scissors = document.getElementById("scissors").onclick = () => {
     makeMove("scissors")
 };
+const reset = document.getElementById("reset").onclick = () => {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    console.log(score);
+}
 
 /* a function to pick our move and decide if we win, lose, or tie with the computer*/
 function makeMove(playerMove) {
@@ -15,16 +29,20 @@ function makeMove(playerMove) {
     /* if-statement to decide if we win lose or tie*/
     if (playerMove === computerMove) {
         console.log('tie');
+        score.ties++;
     } else if (
         (playerMove === "rock" && computerMove === "scissors") ||
         (playerMove === "scissors" && computerMove === "paper") ||
         (playerMove === "paper" && computerMove === "rock")
     ) {
         console.log("You win");
+        score.wins++;
     } else {
         console.log("You lose");
+        score.losses++;
     }
     console.log(`You played ${playerMove}, Computer played ${computerMove}`);
+    console.log(score);
 }
 
 /* a function to pick the computer move*/
