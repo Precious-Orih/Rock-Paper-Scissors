0e2a5e2cb9a509984d0676496be741c0ed417c97
Ehi-Orih <ehiorih25@gmail.com>
2026-07-10 11:36:58 +0100

    Add readme file with algorithm steps and fix the code to enable the game to work on console

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
+/* get buttons for the moves from the html and give them onclick events corresponding to the moves*/
+const rock = document.getElementById("rock").onclick = () => {
+    makeMove("rock")
+};
+const paper = document.getElementById("paper").onclick = () => {
+    makeMove("paper")
+};
+const scissors = document.getElementById("scissors").onclick = () => {
+    makeMove("scissors")
+};
 
-rock.onclick = function rockMove() {
-    console.log('rock')
-    console.log(computerMove);
-}
-paper.onclick = function rockMove() {
-    console.log('paper')
-    console.log(computerMove);
-}
-scissors.onclick = function rockMove() {
-    console.log('scissors')
-    console.log(computerMove);
-}
-
-const random = Math.random()
-let computerMove = '';
-if (random < 1/3) {
-    computerMove = "rock";
-} else if (random < 2/3) {
-    computerMove = "paper";
-} else {
-    computerMove = "scissors";
+/* a function to pick our move and decide if we win, lose, or tie with the computer*/
+function makeMove(playerMove) {
+    const computerMove = pickComputerMove();
+    /* if-statement to decide if we win lose or tie*/
+    if (playerMove === computerMove) {
+        console.log('tie');
+    } else if (
+        (playerMove === "rock" && computerMove === "scissors") ||
+        (playerMove === "scissors" && computerMove === "paper") ||
+        (playerMove === "paper" && computerMove === "rock")
+    ) {
+        console.log("You win");
+    } else {
+        console.log("You lose");
+    }
+    console.log(`You played ${playerMove}, Computer played ${computerMove}`);
 }
 
- 
+/* a function to pick the computer move*/
+function pickComputerMove() {
+    /* generate a random number between zero and one*/
+    const random = Math.random();
+    let computerMove; /* variable to store computer's move*/
+    /* generate a move depending on where the number falls between zero and one*/
+    if (random < (1/3)) {
+        computerMove = "rock";
+    } else if (random < (2/3)) {
+        computerMove = "paper";
+    } else {
+        computerMove = "scissors";
+    } 
+    /* return the value in computerMove*/
+    return computerMove;
+}
---
+++ readMe.md
@@ -0,0 +1,6 @@
+Algorithm for game:
+Player and computer make a move
+Code checks the move
+If the move is the same, it is a tie
+If not, the code checks which one wins
+Score board is updated
